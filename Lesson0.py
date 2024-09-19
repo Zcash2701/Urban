3b60672758cd4ecf9e1aedc70eaea25d8de1ece0
SMex <41436639+Zcash2701@users.noreply.github.com>
2024-09-19 18:13:06 +0300

    Rdy HomeWork

---
+++ Lesson0.py
@@ -0,0 +1,22 @@
+#1st program
+a = 9
+b = 5
+extent = 0.5
+result = (a ** extent) * b
+print('{} ** {} * {} = {}'.format(a, extent, b, result), end='\n\n')
+
+#2st program
+print(9.99 > 9.98 and 1000 != 1000.1, end='\n\n')
+
+#3rd program
+first_sum = 2 * 2 + 2
+second_sum = 2 * (2 + 2)
+result = first_sum == second_sum
+print('{} == {} != {}'.format(first_sum, second_sum, not(result))) # :)
+print(result, end='\n\n')
+
+#4rd program
+str_text = '123.456'
+number = float(str_text)
+number = int(((number - int(number)) * 10))
+print(number)
