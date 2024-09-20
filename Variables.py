1fa7b6d7d1c744e71b1920af9041037fb6c57df6
SMex <41436639+Zcash2701@users.noreply.github.com>
2024-09-20 12:28:26 +0300

    Variables

---
+++ Variables.py
@@ -0,0 +1,5 @@
+count_home_work = 12
+need_time = 1.5
+course_name = 'Python'
+time_to_one_task = need_time / count_home_work
+print('Курс: "{}", всего задач: {}, затрачено часов: {}, среднее время выполнения {} часа.'.format(course_name, count_home_work, need_time, time_to_one_task))
