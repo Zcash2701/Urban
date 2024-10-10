82e942d814508c674dc1fa725f71ae0d7da8ddab
SMex <41436639+Zcash2701@users.noreply.github.com>
2024-10-10 09:50:23 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,7 +1,9 @@
 numb_list = [42, 69, 322, 13, 10, -99, 5, 9, 8, 7, -6, 5]
 count = 0
 while count < len(numb_list):
-    if numb_list[count] <= 0:
+    if numb_list[count] == 0:
+        continue
+    elif numb_list[count] < 0:
         break
     else:
         print(numb_list[count])
