9650980201da226a4fddbb2970689f04484c9075
SMex <alexansdr.kireev@gmail.com>
2024-11-08 23:08:23 +0300

    Форматирование строк

---
+++ Module_7/modul_7_4.py
@@ -0,0 +1,16 @@
+team1_num = 5
+team2_num = 6
+print('В команде Мастера кода участников: %(vol)s ! ' % {'vol': team1_num})
+print('В команде Мастера кода участников: %(vol1)s и %(vol2)s ! ' % {'vol1': team1_num, 'vol2': team2_num})
+
+score_2 = 42
+team2_time = 18015.2
+print("Команда Волшебники данных решила задач: {} !".format(score_2))
+print("Команда Волшебники данных решила задачи за {} !".format(team2_time))
+
+
+score_1 = 40
+challenge_result = 'Мастера кода'
+print(f'Команды решили {score_1} и {score_2} задачи')
+print(f'Результат битвы: победа команды {challenge_result} !')
+
